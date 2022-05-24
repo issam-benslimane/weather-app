48b9558f2d91829e41e53f5d1a06f9750e42d353
issam <issambenslimane9999@hotmail.com>
2022-05-24 18:34:59 +0100

    initial commit, Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "todo list",
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
